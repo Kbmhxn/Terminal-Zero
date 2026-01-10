ffd36b4f735e15a4fd6e3116db7a6d7d1ab66243
Kbmhxn <kbmhxnonyt@gmail.com>
2026-01-10 10:42:37 +0100

    Refactor event listeners and add new functions

--- script.js
+++ script.js
@@ -3,22 +3,17 @@ const loadingScreen = document.getElementById("loadingScreen");
 const menuScreen = document.getElementById("menuScreen");
 const moduleScreen = document.getElementById("moduleScreen");
 
-document.getElementById("startBtn").addEventListener("click", () => {
-  console.log("Start pressed");
-
+document.getElementById("startBtn").onclick = () => {
   startScreen.classList.add("hidden");
   loadingScreen.classList.remove("hidden");
 
   setTimeout(() => {
-    console.log("Loading finished");
     loadingScreen.classList.add("hidden");
     menuScreen.classList.remove("hidden");
   }, 2000);
-});
+};
 
 function openModule(id) {
-  console.log("Opening module:", id);
-
   menuScreen.classList.add("hidden");
   moduleScreen.classList.remove("hidden");
 
@@ -26,11 +21,20 @@ function openModule(id) {
     m.classList.add("hidden")
   );
 
-  const target = document.getElementById(id);
-  if (target) target.classList.remove("hidden");
+  document.getElementById(id).classList.remove("hidden");
 }
 
-function backToMenu() {
+function back() {
   moduleScreen.classList.add("hidden");
   menuScreen.classList.remove("hidden");
+}
+
+function fakeRun() {
+  const c = document.getElementById("execConsole");
+  c.innerHTML += "Simulating execution...\n";
+}
+
+function fakeScan() {
+  const c = document.getElementById("virusConsole");
+  c.innerHTML += "Fake threat detected and logged\n";
 }
